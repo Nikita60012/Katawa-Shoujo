a94f066859e5d0627889d4bd13ceef229227abfc
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2022-12-13 08:30:57 +0300

    Add files via upload

--- KatawaShoujo/core/src/com/mygdx/game/TextImport.java
+++ KatawaShoujo/core/src/com/mygdx/game/TextImport.java
@@ -1,15 +1,20 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
 
 public class TextImport {
    public String[] text = new String[41];
     public String[] reader() {
+        String path = System.getProperty("user.dir") + "/assets/Dialogs/prologueText/dialogs.txt";
+        path = path.replaceAll("\\\\","/");
         StringBuffer input = new StringBuffer();
-        try (FileReader fr = new FileReader("D:/Users/Никита/Desktop/KatawaShoujo/assets/Dialogs/prologueText/dialogs.txt")) {
-           int textChar;
+        try (FileReader fr = new FileReader(path)) {
+            int textChar;
            while ((textChar = fr.read()) != -1){
                input.append((char) textChar);
            }
